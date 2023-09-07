c5b9124433117f26aaa000cb768d76e0c11fe4c6
brikhotso <brenda.rikhotso@gmail.com>
2023-09-07 08:12:30 -0700

    added description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
-
+#inlcude <stdio.h>
+/**
+ * main -Entry point
+ *
+ * put (string) - writes a string to stdout followed by new line
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
