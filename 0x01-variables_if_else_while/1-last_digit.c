e0bcb2dd70ae4afe6e83749fe836da9911a5bc5a
brikhotso <brenda.rikhotso@gmail.com>
2023-09-08 08:19:39 -0700

    first commit string project

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Determine whether last digit of a string is greater than 5
+ * Equal to zero
+ * Less than 6 and not zero
+ * printf - print variable, string  followed by true satement
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ((n % 10) == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	else if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+		       n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, n % 10);
+	}
+	return (0);
+}
