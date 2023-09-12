761e4a845b14c39dceb7556ac067838c75e07978
brikhotso <brenda.rikhotso@gmail.com>
2023-09-12 10:23:23 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function check for alphabet characters
+ *
+ * @c: character to check
+ *
+ * Return: 1 if c is an alphabet, else return 0 if false
+ *
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
