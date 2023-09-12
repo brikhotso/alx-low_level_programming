d7c0db38fdef3b203ec80ba59f4b73d0b51a6268
brikhotso <brenda.rikhotso@gmail.com>
2023-09-12 09:27:01 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - fuction print alphabets 10 times
+ *
+ * print alphabels 10 times in lowercase, followed by new line
+ *
+ * Return: 0 Always (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int count;
+
+	while (count < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
