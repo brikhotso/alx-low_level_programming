3fc335b5e5c8c9613ac20a8033c4ace9cb15b4d5
brikhotso <brenda.rikhotso@gmail.com>
2023-09-12 16:04:12 -0700

    insert stdio.h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 -  prints all natural numbers from n to 98
  *
- *@n: integer to check
+ *@n: integer to count from to 98
  *
  * Return: 0 Always (Success)
  */
