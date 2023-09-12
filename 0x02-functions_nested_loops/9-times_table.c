cf3555a5d02ca40bdae1525c61a0c10abac6a628
brikhotso <brenda.rikhotso@gmail.com>
2023-09-12 14:10:11 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * time_table - function prints the 9 times table, starting with 0.
+ *
+ * Return: )0 Always (Success)
+ */
+void time_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int prod = j * i;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (prod <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
