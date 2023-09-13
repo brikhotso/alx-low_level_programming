2a3b7c4aff704681a1f9c328ba1ca6b9ff1eecd3
brikhotso <brenda.rikhotso@gmail.com>
2023-09-12 19:18:07 -0700

    reduced liness used in function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,8 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int i, j;
+
 	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
@@ -21,24 +21,21 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar('0');
-				}
-				else if (prod <= 9)
+				} else if (prod <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(prod + '0');
-				}
-				else if (prod > 99)
+				} else if (prod > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(prod / 100 + '0');
 					_putchar((prod / 10) % 10 + '0');
 					_putchar(prod % 10 + '0');
-				}
-				else
+				} else
 				{
 					_putchar(',');
 					_putchar(' ');
