c9043835ae835e9317f197e785d2f552c87ca87e
brikhotso <brenda.rikhotso@gmail.com>
2023-09-12 17:52:49 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_times_table - function prints the n times table, starting with 0.
+ *
+ * Return: )0 Always (Success)
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int n;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int prod = j * i;
+
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				else if (prod <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + '0');
+				}
+				else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(prod / 100 + '0');
+					_putchar((prod / 10) % 10 + '0');
+					_putchar(prod % 10 + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod / 10 + '0');
+					_putchar(prod % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
