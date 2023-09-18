460593d563fb18cea85b082f23c1521138e55ea5
brikhotso <brenda.rikhotso@gmail.com>
2023-09-18 04:16:41 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ *
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int b;
+
+	for (b = (_strlen - 1); b <= (_strlen - 1); b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
