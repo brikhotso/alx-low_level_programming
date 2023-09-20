76b240b028c472cbce2f53c19f549af787a278d6
brikhotso <brenda.rikhotso@gmail.com>
2023-09-20 13:35:02 -0700

    debug

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,25 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		       i++;
-
-		       if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
-			   str[i - 1] == '\n' || str[i - 1] == ',' ||
-			   str[i - 1] == '!' || str[i - 1] == ';' ||
-			   str[i - 1] == '?' || str[i - 1] == '"' ||
-			   str[i - 1] == ')' || str[i - 1] == '(' ||
-			   str[i - 1] == '{' || str[i - 1] == '}' ||
-			   str[i - 1] == '.' || i == 0)
-			       str[i] -= 32;
-		       i++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0)
+				str[i] -= 32;
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == str[i - 1])
+						str[i] -= 32;
+				}
+			}
+		}
+		i++;
 	}
 	return (str);
 }
