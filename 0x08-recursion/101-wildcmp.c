5eb070c900e87e6a80010358d45adc547a876491
brikhotso <brenda.rikhotso@gmail.com>
2023-09-27 14:34:17 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,14 +15,8 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-		{
-			return (1);
-		}
+		return (wildcmp (s1, s2 + 1) ||
+			(*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
 	if (*s1 == *s2)
 	{
