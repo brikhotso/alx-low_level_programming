ea88ca61d2617b8e1abf5b8524d50114af94898b
brikhotso <brenda.rikhotso@gmail.com>
2023-09-27 08:10:57 -0700

    initial commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the value of x raised to the power y
+ * @x: base number
+ * @y: power number
+ *
+ * Return: value of x to the power y, for y less than 0 return -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (-1);
+}
