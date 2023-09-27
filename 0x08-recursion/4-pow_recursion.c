010d3252589defb2b7bfee4990dfd3cdb501fa30
brikhotso <brenda.rikhotso@gmail.com>
2023-09-27 08:30:28 -0700

    insert base case

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,9 +9,16 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
-	return (-1);
+	else
+	{
+		return (-1);
+	}
 }
