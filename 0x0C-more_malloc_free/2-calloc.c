c6746caf725dc69b5a170cbd73ae5829a045030f
brikhotso <brenda.rikhotso@gmail.com>
2023-10-05 05:15:24 -0700

    debug code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *memory;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -22,9 +22,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (memory == NULL)
 		return (NULL);
 
-	while (i < (nmemb * size))
+	for (i  = 0; i < nmemb * size; i++)
 	{
-		i++;
 		memory[i] = '\0';
 	}
 
