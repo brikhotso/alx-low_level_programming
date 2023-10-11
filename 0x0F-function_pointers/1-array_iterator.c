23c8a597c14fd31058c85545bb928eb9d3a80684
brikhotso <brenda.rikhotso@gmail.com>
2023-10-11 06:43:32 -0700

    handle arguments

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL || action == NULL)
+	if (array == NULL || action == NULL || size <= 0)
 		return;
 
 	for (i = 0; i < size; i++)
