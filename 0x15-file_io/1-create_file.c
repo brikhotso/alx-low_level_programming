9be89947e3046b08ed8ee59fd589257b981ff88d
brikhotso <brenda.rikhotso@gmail.com>
2023-10-30 12:51:52 -0700

    fixed code to write to the file instead of stdin

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	while (text_content[len] != '\0')
 	{
-		writefile = write(STDIN_FILENO, text_content, len);
+		writefile = write(openfile, text_content, len);
 
 		if (text_content == NULL)
 		{
