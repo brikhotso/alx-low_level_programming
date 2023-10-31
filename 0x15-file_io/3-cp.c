03a8fe7cb816b240d66b5f18e6f1988575e9616f
brikhotso <brenda.rikhotso@gmail.com>
2023-10-31 06:26:53 -0700

    error handling

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -101,7 +101,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
