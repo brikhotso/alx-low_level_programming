0f97c5c9b49888799bcf4a6d2d89c371c8811941
brikhotso <brenda.rikhotso@gmail.com>
2023-11-01 10:13:05 -0700

    displaying infomation in an ELF File

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -163,7 +163,8 @@ void display_osabi(unsigned char *e_ident)
  */
 void display_abi(unsigned char *e_ident)
 {
-	printf("  ABI Version:                       %d\n", e_ident[EI_ABIVERSION]);
+	printf("  ABI Version:                       %d\n",
+	       e_ident[EI_ABIVERSION]);
 }
 
 /**
