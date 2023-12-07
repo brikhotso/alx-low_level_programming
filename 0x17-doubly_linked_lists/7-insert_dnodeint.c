edb6665f21452a036271ed3be8152220c845b513
brikhotso <brenda.rikhotso@gmail.com>
2023-12-07 12:11:12 -0800

    handle fail

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count = 0;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (h == NULL && new_node == NULL)
+	if (h == NULL || new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
