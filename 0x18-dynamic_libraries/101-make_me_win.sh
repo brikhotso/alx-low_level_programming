df0e13433679789ecb5c401bf10f25d462401f7b
brikhotso <brenda.rikhotso@gmail.com>
2023-12-11 08:45:05 -0800

    you win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/brikhotso/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD="/tmp/win.so"
+wget -P /tmp/ https://raw.github.com/brikhotso/alx-low_level_programming/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
